47632c080b9a83348b88c2ad3983273e788ac644
Alok Ranjan <ranjan_alok@network.lilly.com>
2016-12-14 20:39:04 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.get('/sitemap', function(req, res, next) {
     path.forEach(function(i) {
         urls.push({
             url : '/' + i,
-            lastmod : fs.statSync(proc.env.HOME + "/sitemap-poc/views/" + i).mtime,
+            lastmod : fs.statSync("/app/views/" + i).mtime,
             changefreq : 'daily',
             priority : 0.5
         });
